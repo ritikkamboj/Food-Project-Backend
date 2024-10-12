d5a223e58f8bde89c473126754e7813f0f18b654
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2024-10-12 12:25:11 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const app = express();
+
+// creating route and server
+
+app.get("/", (req, res) => {
+    res.status(200).send('<h1> Welcome to the backend project </h1>');
+});
+
+const PORT = 8080;
+
+app.listen(PORT, () => {
+    console.log('server is running');
+
+})
