37888492b0d1e4cb0697dc11b9562de827e85629
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2024-10-12 17:38:21 +0530

    use all middlewares and don't use semicolon in .env file

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 const express = require("express");
+const morgan = require('morgan');
+const colors = require('colors');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+dotenv.config();
+
 
 const app = express();
+
+// middlewares 
+
+app.use(cors());
+app.use(morgan('dev'))
+app.use(express.json());
+// app.use();
+
 
 // creating route and server
 
@@ -8,9 +23,9 @@ app.get("/", (req, res) => {
     res.status(200).send('<h1> Welcome to the My backend project </h1>');
 });
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
-    console.log('server is running');
+    console.log(`server is running on ${PORT}`.america.green);
 
 })
