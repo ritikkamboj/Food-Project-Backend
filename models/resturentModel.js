a3f74b226e9e63a83a05b6232c1bc4726ecaef67
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2024-10-17 13:09:14 +0530

    created and exported the resturent Schema

---
+++ models/resturentModel.js
@@ -0,0 +1,66 @@
+const mongoose = require("mongoose");
+
+const resturentSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: [true, "Title is required"],
+        },
+        imageUrl: {
+            type: String,
+        },
+        foods: {
+            type: Array,
+        },
+        time: {
+            type: String,
+        },
+        pickup: {
+            type: Boolean,
+            default: true,
+        },
+        delivery: {
+            type: Boolean,
+            default: true,
+        },
+        isOpen: {
+            type: Boolean,
+            default: true,
+        },
+        logoUrl: {
+            type: String,
+        },
+        rating: {
+            type: Number,
+            default: 1,
+            max: 5,
+            min: 1,
+        },
+        ratingCount: {
+            type: String,
+        },
+        code: {
+            type: String,
+        },
+        coords: {
+            id: { type: String },
+            latitude: { type: Number },
+            latitudeDelta: { type: Number },
+            longitude: { type: Number },
+            longitudeDelta: { type: Number },
+            address: {
+                type: String
+            },
+            title: { type: String }
+
+
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const resturent = mongoose.model("resturent", resturentSchema);
+
+module.exports = resturent;
